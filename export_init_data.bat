4d89d35d36a2a80843fc80c4da7392ca47d0182b
DriveSoft <support@drive-software.com>
2021-05-18 20:31:08 +0300

    init data added

--- export_init_data.bat
+++ export_init_data.bat
@@ -2,5 +2,7 @@ python manage.py dumpdata --natural-foreign auth.group > 001auth.group.json
 python manage.py dumpdata citytree.PlaceType citytree.IrrigationMethod citytree.Status citytree.CareType citytree.Remark citytree.groupspec citytree.typespec > 002dict.json
 python manage.py dumpdata citytree.species -o 003species.json
 python manage.py dumpdata coregis.corecity -o 004city.json
+python manage.py dumpdata roadaccident.Maneuver roadaccident.TypeViolation roadaccident.Violator > 005roaddict.json
+
 
 
